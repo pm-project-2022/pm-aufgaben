5458f8426f07846fa4955c0b11527dee6bc71b57
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-04-16 19:34:28 +0200

    description added

--- vorgaben/factory_method_pattern/src/Drive.java
+++ vorgaben/factory_method_pattern/src/Drive.java
@@ -1,11 +1,17 @@
 public abstract class Drive {
     private int storageSize;
+    private String description;
 
-    public Drive(int storageSize) {
+    public Drive(int storageSize, String description) {
         this.storageSize = storageSize;
+        this.description = description;
     }
 
     public int getStorageSize() {
         return storageSize;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 }
