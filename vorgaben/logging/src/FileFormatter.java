307524955dd736e1d9d6b9d9e71b3f36926d2487
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-04-19 05:51:24 +0200

    initial commit

--- vorgaben/logging/src/FileFormatter.java
+++
@@ -1,9 +0,0 @@
-import java.util.logging.LogRecord;
-import java.util.logging.SimpleFormatter;
-
-public class FileFormatter extends SimpleFormatter{
-    @Override
-    public String format(LogRecord record){
-        return record.getLoggerName() + "," + record.getLevel() + "," + record.getSourceMethodName() + "," + record.getSourceClassName() + "," + record.getMessage() + "\n";
-    }  
-}
