11439d92684deba2f44456057922adf976ff0b07
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-04-27 15:51:09 +0200

    monsterspawn added

--- dungeon/code/desktop/src/desktop/MyGame.java
+++ dungeon/code/desktop/src/desktop/MyGame.java
@@ -1,19 +1,40 @@
 package desktop;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
 
-import EntityController.MyHero;
+import EntityController.Hero.MyHero;
+import EntityController.Monster.Monster;
+import EntityController.Monster.NormalMonster.Chort;
+import EntityController.Monster.NormalMonster.NormalMonsterNameList;
+import EntityController.Monster.NormalMonster.NormalMonsterQuantity;
+import EntityController.Monster.SmallMonster.Imp;
+import EntityController.Monster.SmallMonster.SmallMonsterNameList;
+import EntityController.Monster.SmallMonster.SmallMonsterQuantity;
+import EntityController.MovementBehaviour.SimpleMovementBehaviour.PatrolY;
 import controller.MainController;
 import level.generator.LevelLoader.LevelLoader;
 import level.generator.dungeong.graphg.NoSolutionException;
 
 public class MyGame extends MainController {
     private MyHero hero;
+    private SmallMonsterNameList smallMonsterNames;
+    private NormalMonsterNameList normalMonsterNames;
+    private ArrayList<Monster> smallMonsterList;
+    private ArrayList<Monster> normalMonsterList;
+    private int floor;
 
     @Override
     protected void setup() {
         levelAPI.setGenerator(new LevelLoader());
-        hero = new MyHero(painter, batch);
+        hero = new MyHero(painter, batch);     
+        this.smallMonsterNames = new SmallMonsterNameList();
+        this.normalMonsterNames = new NormalMonsterNameList();  
+        this.smallMonsterList = new ArrayList<>();
+        this.normalMonsterList = new ArrayList<>();
+        this.floor = 1;
+
         try {
             levelAPI.loadLevel();
         } catch (NoSolutionException e) {
@@ -26,12 +47,16 @@ public class MyGame extends MainController {
     }
 
     @Override
-    protected void beginFrame() {}
+    protected void beginFrame() {
+
+    }
 
     @Override
     protected void endFrame() {
         if (levelAPI.getCurrentLevel().isOnEndTile(hero)) {
             try {
+                entityController.clear();
+                entityController.add(hero);
                 levelAPI.loadLevel();
             } catch (NoSolutionException e) {
                 e.printStackTrace();
@@ -42,6 +67,45 @@ public class MyGame extends MainController {
     @Override
     public void onLevelLoad() {
         hero.setLevel(levelAPI.getCurrentLevel());
+        initMonster();
+
+    }
+
+    private void initMonster() {
+        this.smallMonsterList.clear();
+        this.normalMonsterList.clear();
+        initSmallMonster();
+        initNormalMonster();
+        System.out.println("Small:" + this.smallMonsterList.size() + "\nNormal:" + this.normalMonsterList.size() + "\n");
+        this.floor += 1;
+    }
+
+    private void initSmallMonster(){
+        for(int i = 0; i < new SmallMonsterQuantity(this.floor).getQuantity(); i++) {
+            int ranMon = 0;
+            this.smallMonsterList.add(monFac(this.smallMonsterNames.getMonsterList().get(ranMon)));
+            entityController.add(this.smallMonsterList.get(i));
+            this.smallMonsterList.get(i).setLevel(levelAPI.getCurrentLevel());
+        }
+    }
+
+    private void initNormalMonster(){
+        for(int i = 0; i < new NormalMonsterQuantity(this.floor).getQuantity(); i++) {
+            int ranMon = 0;
+            this.normalMonsterList.add(monFac(this.normalMonsterNames.getMonsterList().get(ranMon)));
+            entityController.add(this.normalMonsterList.get(i));
+            this.normalMonsterList.get(i).setLevel(levelAPI.getCurrentLevel());
+        }
+    }
+
+    private Monster monFac(String monName) {
+        if(monName.equals("Imp")){
+            return new Imp(painter, batch, this.hero, this.floor, new PatrolY());
+        }
+        if(monName.equals("Chort")){
+            return new Chort(painter, batch, hero, this.floor, new PatrolY());
+        }
+        return null;
     }
 
     public static void main(String[] args) {
