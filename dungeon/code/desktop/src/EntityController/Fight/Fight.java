64d5a3e160dada564934478e42fe07c2ea3ea932
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-04-29 00:14:47 +0200

    doc added

--- dungeon/code/desktop/src/EntityController/Fight/Fight.java
+++ dungeon/code/desktop/src/EntityController/Fight/Fight.java
@@ -2,45 +2,80 @@ package EntityController.Fight;
 
 import java.util.Random;
 
-import EntityController.Hero.MyHero;
 import EntityController.Monster.Monster;
+
+/**
+ * Calculates a fight/attack between hero and monster
+ */
 
 public class Fight {
     Monster monster;
+    
+    //hitchance
     int hit;
+
+    //damage dealt
     int dmg;
+
     Random random;
 
-    public Fight(Monster monster){
+    /**
+     * Constructor
+     * @param monster attacked by the hero
+     */
+    public Fight(Monster monster) {
         this.monster = monster;
         this.random = new Random();
     }
 
-    public void fight(){
-        if(this.monster.getHero().getStats().getAccuracy() > this.monster.getStats().getEvasion()){
-            calculateDamage();
-        }else{
-            calculateHit();
+    /**
+     * starts the fight
+     * @return dealt damage or not
+     */
+
+    public boolean fight() {
+        if (this.monster.getHero().getStats().getAccuracy() > this.monster.getStats().getEvasion()) {
+            return calculateDamage();
+        } else {
+            return calculateHit();
         }
     }
 
-    private void calculateHit() {
-        this.hit = random.nextInt(2+1) + 1;
-        if(this.hit == 1){
-            calculateDamage();
+    /**
+     * calculate if the hit misses or not
+     * @return hit miss or not
+     */
+
+    private boolean calculateHit() {
+        this.hit = random.nextInt(2 + 1) + 1;
+        if (this.hit == 1) {
+            return calculateDamage();
         }
+        return false;
     }
 
-    private void calculateDamage(){
-        if(this.monster.getHero().getStats().getStrength() > this.monster.getStats().getdefense()){
-            this.dmg = (this.monster.getStats().getdefense() - this.monster.getHero().getStats().getStrength()) - this.monster.getStats().getHealhtPoints();
+    /**
+     * calculates damage if the hit was successfull
+     * @return true
+     */
 
-            if(this.dmg >= 1){
-                this.monster.getStats().setHealhtPoints(this.dmg);
-            }else{
+    private boolean calculateDamage() {
+        if (this.monster.getHero().getStats().getStrength() > this.monster.getStats().getdefense()) {
+            this.dmg = (this.monster.getStats().getdefense() - this.monster.getHero().getStats().getStrength());
+            int monHP = this.monster.getStats().getHealhtPoints() + this.dmg;
+            if (monHP < 0) {
                 this.monster.getStats().setHealhtPoints(0);
+                System.out.println(this.monster.getStats().toString());
+
+            } else {
+                this.monster.getStats().setHealhtPoints(monHP);
+                System.out.println(this.monster.getStats().toString());
             }
+        } else {
+            this.monster.getStats().setHealhtPoints(this.monster.getStats().getHealhtPoints() - 1);
+            System.out.println(this.monster.getStats().toString());
         }
-        System.out.println(this.monster.getStats().toString());
+        return true;
+
     }
 }
