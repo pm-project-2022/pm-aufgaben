fe2f7942b15409ead59b3bdb8a6d540281b714d4
jrau1801 <janrau1802@gmail.com>
2022-05-04 12:32:53 +0200

    trap animations

--- dungeon/code/desktop/src/Traps/Traps.java
+++ dungeon/code/desktop/src/Traps/Traps.java
@@ -2,6 +2,7 @@ package Traps;
 
 import Entities.Items.Item;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import graphic.Animation;
 import graphic.Painter;
 
 import java.util.ArrayList;
@@ -15,6 +16,9 @@ public class Traps extends Item {
 
     private void setAnimation() {
         ArrayList<String> animation = new ArrayList<String>();
+        animation.add("traps/floor_spikes_anim_f0.png");
+        animation.add("traps/floor_spikes_anim_f1.png");
+        animation.add("traps/floor_spikes_anim_f2.png");
         animation.add("traps/floor_spikes_anim_f3.png");
         initIdleAnimation(animation);
     }
