5fef257e58e2aff01c1c52dd046b8d5143ef2ad5
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-05 20:30:47 +0200

    removed obsolete imports and spaces

--- dungeon/code/desktop/src/desktop/DesktopLauncher.java
+++ dungeon/code/desktop/src/desktop/DesktopLauncher.java
@@ -1,6 +1,6 @@
 package desktop;
 
-import Gui.Gui;
+
 import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.DungeonApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
@@ -8,19 +8,9 @@ import controller.LibgdxSetup;
 import controller.MainController;
 import tools.Constants;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public final class DesktopLauncher {
     public static void run(MainController mc) {
-
-
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = Constants.WINDOW_WIDTH;
         config.height = Constants.WINDOW_HEIGHT;
