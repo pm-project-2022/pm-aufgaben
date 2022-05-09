f4eb4df9b5bd1221b591e563137bf62ec1b5f693
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 16:50:31 +0200

    attributes encapsuled

--- vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/Article.java
+++ vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/Article.java
@@ -2,12 +2,30 @@ package bad_smells_und_refactoring;
 
 public class Article {
 
-    public Bike bike;
-    public int purchaseAmount;
+    private Bike bike;
+    private int purchaseAmount;
 
     public Article(Bike b, int pa) {
         bike = b;
         purchaseAmount = pa;
     }
+
+
+    public Bike getBike() {
+        return this.bike;
+    }
+
+    public void setBike(Bike bike) {
+        this.bike = bike;
+    }
+
+    public int getPurchaseAmount() {
+        return this.purchaseAmount;
+    }
+
+    public void setPurchaseAmount(int purchaseAmount) {
+        this.purchaseAmount = purchaseAmount;
+    }
+
 
 }
