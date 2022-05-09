d9fa079cad6037f82acb2fdf1899be982eb0b7de
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 16:33:25 +0200

    set and get batterycapacity test added

--- vorgaben/bad_smells_und_refactoring/app/src/test/java/bad_smells_und_refactoring/EbikeTest.java
+++ vorgaben/bad_smells_und_refactoring/app/src/test/java/bad_smells_und_refactoring/EbikeTest.java
@@ -1,17 +1,19 @@
 package bad_smells_und_refactoring;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class EbikeTest {
 
-    public static EBike eBike;
+    public EBike eBike;
 
-    @BeforeClass
-    public static void setUp(){
-        eBike = new EBike("Ebike b1", 599.99, 30, 1, 1,20);
+    @Before
+    public void setUp(){
+        this.eBike = new EBike("Ebike b1", 599.99, 30, 1, 1,20);
     }
 
     /**
@@ -20,6 +22,25 @@ public class EbikeTest {
     @Test
     public void testEBike(){
         assertNotNull(eBike);
+    }
+
+    /**
+     * Lässt sich die Batteriekapazität zurückgeben
+     */
+    @Test
+    public void testGetBatteryCapacity(){
+        Integer expected = 20;
+        assertEquals(expected, this.eBike.getBatteryCapacity());
+    }
+
+    /**
+     * Lässt sich ein neuer Wert für die Batteriekapazität setzen
+     */
+    @Test
+    public void testSetBatteryCapacity(){
+        Integer oldBatteryCapacity = this.eBike.getBatteryCapacity();
+        this.eBike.setBatteryCapacity(25);
+        assertNotEquals(oldBatteryCapacity, this.eBike.getBatteryCapacity());
     }
     
 }
