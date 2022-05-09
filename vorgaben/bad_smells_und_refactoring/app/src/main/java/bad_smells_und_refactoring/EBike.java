58123c70809cc47778838ec653951c5ca223702a
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 16:29:21 +0200

    attributes encapsulated and setter/getter added

--- vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/EBike.java
+++ vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/EBike.java
@@ -1,11 +1,22 @@
 package bad_smells_und_refactoring;
 
 public class EBike extends Bike {
-    public Integer batteryCapacity;
+    private Integer batteryCapacity;
 
     public EBike(String pn, double p, int ms, int rgc, int fgc, int batteryCapacity) {
         super(pn, p, ms, rgc, fgc);
         this.batteryCapacity = batteryCapacity;
     }
 
+    public void setBatteryCapacity(Integer batteryCapacity){
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    public Integer getBatteryCapacity() {
+        return this.batteryCapacity;
+    }
+
 }
+
+    
+
