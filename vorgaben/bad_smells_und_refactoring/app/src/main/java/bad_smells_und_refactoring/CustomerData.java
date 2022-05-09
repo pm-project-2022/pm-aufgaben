b22e9b626306a6712546b92bb4d2ebdfa4422e1f
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 17:56:21 +0200

    attributes encapsulated and setter/getter added

--- vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/CustomerData.java
+++ vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/CustomerData.java
@@ -3,14 +3,14 @@ package bad_smells_und_refactoring;
 import java.util.Date;
 
 public class CustomerData {
-    public String customerName;
-    public String nickname;
-    public Date birthday;
-    public String email;
-    public String street;
-    public String streetNumber;
-    public int postalCode;
-    public String city;
+    private String customerName;
+    private String nickname;
+    private Date birthday;
+    private String email;
+    private String street;
+    private String streetNumber;
+    private int postalCode;
+    private String city;
 
     public CustomerData(String cn, String n, Date b, String e, String s, String sn, int pc,  String c){
         this.customerName = cn;
@@ -22,4 +22,70 @@ public class CustomerData {
         this.postalCode = pc;
         this.city = c;
     }
+
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getNickname() {
+        return this.nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Date getBirthday() {
+        return this.birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return this.street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNumber() {
+        return this.streetNumber;
+    }
+
+    public void setStreetNumber(String streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public int getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
 }
