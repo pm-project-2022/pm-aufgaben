8fc6b365ed4fab3e6b52aae03af7bb2d07628e36
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 15:51:45 +0200

    duplicated code removed

--- vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/Bike.java
+++ vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/Bike.java
@@ -1,17 +1,73 @@
 package bad_smells_und_refactoring;
 
-
 public class Bike {
 
-    public String productName;
-    public double price;
-    public Integer batteryCapacity;
+    private String productName;
+    private double price;
+    private int maxSpeed;
+    private int rearGearsCount;
+    private int frontGearsCount;
+    private Integer batteryCapacity;
 
-    public Integer getBatteryCapacity() {
-        return batteryCapacity;
+    public Bike(String pn, double p, int ms, int rgc, int fgc){
+        this.productName = pn;
+        this.price = p;
+        this.maxSpeed = ms;
+        this.rearGearsCount = rgc;
+        this.frontGearsCount = fgc;
     }
 
+
+    public String getProductName() {
+        return this.productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getRearGearsCount() {
+        return this.rearGearsCount;
+    }
+
+    public void setRearGearsCount(int rearGearsCount) {
+        this.rearGearsCount = rearGearsCount;
+    }
+
+    public int getFrontGearsCount() {
+        return this.frontGearsCount;
+    }
+
+    public void setFrontGearsCount(int frontGearsCount) {
+        this.frontGearsCount = frontGearsCount;
+    }
+
+    public Integer getBatteryCapacity() {
+        return this.batteryCapacity;
+    }
+
+    public void setBatteryCapacity(Integer batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+    
+
     public int getGearsCount() {
-        throw new UnsupportedOperationException("Not Implemented");
+        return this.rearGearsCount * this.frontGearsCount;
     }
 }
--- vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/EBike.java
+++ vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/EBike.java
@@ -1,26 +1,11 @@
 package bad_smells_und_refactoring;
 
 public class EBike extends Bike {
+    
 
-    public int maxSpeed;
-    public int rearGearsCount;
-    public int frontGearsCount;
-
-    public EBike(String pn, double p, int ms, int rgc, int fgc, int bc) {
-        productName = pn;
-        price = p;
-        maxSpeed = ms;
-        rearGearsCount = rgc;
-        frontGearsCount = fgc;
-        batteryCapacity = bc;
+    public EBike(String pn, double p, int ms, int rgc, int fgc) {
+        super(pn, p, ms, rgc, fgc);
+        
     }
 
-    public int getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    @Override
-    public int getGearsCount() {
-        return rearGearsCount * frontGearsCount;
-    }
 }
