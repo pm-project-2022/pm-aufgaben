8a0cc1cb97cce2b69014c223b924768606401a3b
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 17:23:41 +0200

    create bill test added

--- vorgaben/bad_smells_und_refactoring/app/src/test/java/bad_smells_und_refactoring/Billtest.java
+++ vorgaben/bad_smells_und_refactoring/app/src/test/java/bad_smells_und_refactoring/Billtest.java
@@ -1,5 +1,25 @@
 package bad_smells_und_refactoring;
 
-public class Billtest {
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BillTest {
+    
+    private Bill bill;
+
+    @Before
+    public void setUp(){
+        this.bill = new Bill("Max Mustermann", "MaMu", "Musterstrasse", "5", 01234, new Date() , "max@mustermann.de", "Musterstadt");
+    }
+
+    @Test
+    public void getBill(){
+        assertNotNull(this.bill);
+    }
     
 }
