d6df4322eb17b0078807effb4ecdc16c3cf3fe54
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 21:44:25 +0200

    biketyp added

--- vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/Bike.java
+++ vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/Bike.java
@@ -1,20 +1,21 @@
 package bad_smells_und_refactoring;
 
 public class Bike {
-
     private String productName;
     private double price;
     private int maxSpeed;
     private int rearGearsCount;
     private int frontGearsCount;
+    private final String biketyp;
     
 
-    public Bike(String pn, double p, int ms, int rgc, int fgc){
+    public Bike(String pn, double p, int ms, int rgc, int fgc, String bikeType){
         this.productName = pn;
         this.price = p;
         this.maxSpeed = ms;
         this.rearGearsCount = rgc;
         this.frontGearsCount = fgc;
+        this.biketyp = bikeType;
     }
 
 
@@ -57,6 +58,10 @@ public class Bike {
     public void setFrontGearsCount(int frontGearsCount) {
         this.frontGearsCount = frontGearsCount;
     } 
+
+    public String getBikeType(){
+        return this.biketyp;
+    }
 
     public int getGearsCount() {
         return this.rearGearsCount * this.frontGearsCount;
