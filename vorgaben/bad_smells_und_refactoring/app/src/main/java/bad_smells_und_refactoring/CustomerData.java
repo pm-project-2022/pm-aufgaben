47dbbf2b316c611142470f9835b91eefb3478607
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-09 17:53:37 +0200

    initial commit

--- vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/CustomerData.java
+++ vorgaben/bad_smells_und_refactoring/app/src/main/java/bad_smells_und_refactoring/CustomerData.java
@@ -1,5 +1,25 @@
 package bad_smells_und_refactoring;
 
+import java.util.Date;
+
 public class CustomerData {
-    
+    public String customerName;
+    public String nickname;
+    public Date birthday;
+    public String email;
+    public String street;
+    public String streetNumber;
+    public int postalCode;
+    public String city;
+
+    public CustomerData(String cn, String n, Date b, String e, String s, String sn, int pc,  String c){
+        this.customerName = cn;
+        this.nickname = n;
+        this.birthday = b;
+        this.email = e;
+        this.street = s;
+        this.streetNumber = sn;
+        this.postalCode = pc;
+        this.city = c;
+    }
 }
