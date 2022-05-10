ffdb36e75c962d8d62965845aa19cef0ab0455bd
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-10 12:20:28 +0200

    mockito test

--- vorgaben/mocking/app/src/test/java/mocking/UtilityTest.java
+++ vorgaben/mocking/app/src/test/java/mocking/UtilityTest.java
@@ -1,5 +1,14 @@
 package mocking;
 
+import static org.mockito.Mockito.mock;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import mocking.Utility;
+
 // Ergänzen Sie UtilityTest so, dass alle Testmethoden grün werden.
 // Die vorgegebenen Klassen dürfen nicht geändert werden.
 // Die Testaufrufe müssen auch erhalten bleiben.
@@ -7,19 +16,24 @@ public class UtilityTest {
     private Utility utilityClass;
     // Initialisieren Sie die Attribute entsprechend vor jedem Test.
 
+    @Before
+    void setUp(){
+        this.utilityClass = mock(Utility.class);
+    }
+
     @Test
     void test_nonEvilAdd() {
-        Assertions.assertEquals(10, utilityClass.nonEvilAdd(9, 1));
+        Assert.assertEquals(10, utilityClass.nonEvilAdd(9, 1));
     }
 
     @Test
     void test_evilAdd() {
-        Assertions.assertEquals(10, utilityClass.evilAdd(9, 1));
+        Assert.assertEquals(10, utilityClass.evilAdd(9, 1));
     }
 
     @Test
     void test_veryEvilAdd() {
         utilityClass.veryEvilAdd(9, 1);
-        Assertions.assertEquals(10, utilityClass.getIntResult());
+        Assert.assertEquals(10, utilityClass.getIntResult());
     }
 }
