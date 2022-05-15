b25625c817848583bb33b3e38f86e41dce8b5b37
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-15 23:17:06 +0200

    doc added

--- dungeon/code/desktop/src/Entities/Moveable/Monster/MonsterMovement/IMovement.java
+++ dungeon/code/desktop/src/Entities/Moveable/Monster/MonsterMovement/IMovement.java
@@ -4,5 +4,11 @@ import Entities.Moveable.Monster.Monster;
 import Helper.PointBooleanTransmitter;
 
 public interface IMovement {
+
+    /**
+     * determines the new position of a monster depending on their movement
+     * @param monster current monster
+     * @return the new monster position
+     */
     PointBooleanTransmitter getMonsterMovement(Monster monster);
 }
