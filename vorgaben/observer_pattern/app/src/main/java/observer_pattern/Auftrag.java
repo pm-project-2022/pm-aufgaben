de9bf891063e0926e5dbd1091e12904dd4406132
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-16 17:50:39 +0200

    initial commit

---
+++ vorgaben/observer_pattern/app/src/main/java/observer_pattern/Auftrag.java
@@ -0,0 +1,19 @@
+package observer_pattern;
+
+class Auftrag {
+    private WarenTyp warenTyp;
+    private int anzahl;
+
+    public Auftrag(WarenTyp warenTyp, int anzahl) {
+        this.warenTyp = warenTyp;
+        this.anzahl = anzahl;
+    }
+
+    public WarenTyp getWarenTyp() {
+        return warenTyp;
+    }
+
+    public int getAnzahl() {
+        return anzahl;
+    }
+}
