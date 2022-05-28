d1608b4c05db7f07c6c7f33c7cde7f1d0b51aea4
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-29 01:24:35 +0200

    extends Drucker

--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Laserdrucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Laserdrucker.java
@@ -1,5 +1,17 @@
 package Template.Pattern;
 
-public class Laserdrucker {
+public class Laserdrucker extends Drucker {
+
+    @Override
+    protected void scannen() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void drucken() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Tintendrucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Tintendrucker.java
@@ -1,5 +1,17 @@
 package Template.Pattern;
 
-public class Tintendrucker {
+public class Tintendrucker extends Drucker {
+
+    @Override
+    protected void scannen() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    protected void drucken() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
