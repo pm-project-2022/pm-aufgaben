b5fcfaefb8cafd607dd3e63000387a6fd78160bd
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-29 01:23:56 +0200

    logger added

--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Drucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Drucker.java
@@ -1,7 +1,11 @@
 package Template.Pattern;
 
+import java.util.logging.Logger;
+
 public abstract class Drucker {
-    
+    protected Logger log;
+
+
     public final void kopieren(){
         scannen();
         drucken();
