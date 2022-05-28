8c4f50742adc0caa597fe9c7272f17544ae9bb5e
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-29 01:22:22 +0200

    method kopieren and abstract methods scannen/drucken added

--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Drucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Drucker.java
@@ -2,4 +2,10 @@ package Template.Pattern;
 
 public abstract class Drucker {
     
+    public final void kopieren(){
+
+    }
+
+    abstract protected void scannen();
+    abstract protected void drucken();
 }
