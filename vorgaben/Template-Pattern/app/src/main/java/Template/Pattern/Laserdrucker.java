af9b53a98d839ced8415afa936466ee9c408357d
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-29 01:26:15 +0200

    logger initiated

--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Laserdrucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Laserdrucker.java
@@ -1,6 +1,12 @@
 package Template.Pattern;
 
+import java.util.logging.Logger;
+
 public class Laserdrucker extends Drucker {
+
+    public Laserdrucker(){
+        this.log = Logger.getLogger("Laserdruckerlogger");
+    }
 
     @Override
     protected void scannen() {
--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Tintendrucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Tintendrucker.java
@@ -1,6 +1,12 @@
 package Template.Pattern;
 
+import java.util.logging.Logger;
+
 public class Tintendrucker extends Drucker {
+
+    public Tintendrucker(){
+        this.log = Logger.getLogger("Tintendruckerlogger");
+    }
 
     @Override
     protected void scannen() {
