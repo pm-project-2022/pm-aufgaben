0a4c3b0e8c81b4d0be9937063ad08e6df8b2faf0
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-29 01:52:29 +0200

    logging added

--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Main.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Main.java
@@ -1,9 +1,13 @@
 package Template.Pattern;
 
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args) {
+        Logger log = Logger.getLogger(Main.class.getName());
         Drucker tintenDrucker = new Tintendrucker();
         Drucker laserDrucker = new Laserdrucker();
-        
+        log.info(tintenDrucker.kopieren());
+        log.info(laserDrucker.kopieren());
     }
 }
