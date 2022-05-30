c4915d19a2beb98aadb7c5fe20f04c79374bb1be
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-30 18:35:47 +0200

    return hashmap und classloader instanz erstellt. klassen mportiert.

--- vorgaben/reflection/app/src/main/java/reflection/calculator/OperationLoader.java
+++ vorgaben/reflection/app/src/main/java/reflection/calculator/OperationLoader.java
@@ -4,14 +4,16 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.HashMap;
 import java.util.Map;
+
 import java.util.logging.Logger;
 
 import reflection.operations.IOperation;
 
 public class OperationLoader {
     private static Logger log = Logger.getLogger(OperationLoader.class.getName());
-
+    
     /**
      * Loads the operation classes from a directory.
      *
@@ -19,14 +21,16 @@ public class OperationLoader {
      * @return a map with the operations and their names, or an empty map if none were found
      */
     public static Map<String, IOperation> loadOperations(File directory) {
+        Map<String, IOperation> operations = new HashMap<>();
         log.info("Operationen werden eingelesen");
         try {
-            URL[] operations = new URL[]{directory.toURI().toURL()};
-            URLClassLoader ucl = URLClassLoader.newInstance(operations);
-            Class<?> addition = ucl.loadClass("Addition");
-            Class<?> subtraktion = ucl.loadClass("Subtraktion");
-            Class<?> multiplikation = ucl.loadClass("Multiplikation");
-            Class<?> division = ucl.loadClass("Division");
+            
+            URL[] ua = new URL[]{directory.toURI().toURL()};
+            URLClassLoader ucl = URLClassLoader.newInstance(ua);
+            Class<?> classAddition = ucl.loadClass("Addition");
+            Class<?> classSubtraktion = ucl.loadClass("Subtraktion");
+            Class<?> classMultiplikation = ucl.loadClass("Multiplikation");
+            Class<?> classDivision = ucl.loadClass("Division");
         } catch (MalformedURLException e) {
             log.severe("Argument ist null oder das Protokoll ist unbekannt");
         } catch (ClassNotFoundException e) {
