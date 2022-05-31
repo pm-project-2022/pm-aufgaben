7d6fc69d6204d048d0767e6fbe2429f20d9fc9ce
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-05-31 14:55:29 +0200

    Merge branch 'AB7' of https://github.com/pm-project-2022/pm-aufgaben into AB7

--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Laserdrucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Laserdrucker.java
@@ -1,15 +1,20 @@
 package Template.Pattern;
 
+import java.util.logging.Logger;
+
 public class Laserdrucker extends Drucker {
+    Logger log = Logger.getLogger(Main.class.getName());
 
     @Override
     protected String scannen() {
+        log.info("Scanne das Dokument mit dem Laserdrucker.");
         return "Scanne das Dokument mit dem Laserdrucker.";
         
     }
 
     @Override
     protected String drucken() {
+        log.info("Drucke das Dokument auf dem Laserdrucker.");
         return "Drucke das Dokument auf dem Laserdrucker.";
         
     }
--- vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Tintendrucker.java
+++ vorgaben/Template-Pattern/app/src/main/java/Template/Pattern/Tintendrucker.java
@@ -1,17 +1,22 @@
 package Template.Pattern;
 
+import java.util.logging.Logger;
+
 public class Tintendrucker extends Drucker {
+   Logger log = Logger.getLogger(Main.class.getName());
 
-    @Override
-    protected String scannen() {
-       return "Scanne das Dokument mit dem Tintendrucker.";
-        
-    }
+   @Override
+   protected String scannen() {
+      log.info("Scanne das Dokument mit dem Tintendrucker.");
+      return "Scanne das Dokument mit dem Tintendrucker.";
 
-    @Override
-    protected String drucken() {
-       return  "Drucke das Dokument auf dem Tintendrucker.";
-        
-    }
-    
+   }
+
+   @Override
+   protected String drucken() {
+      log.info("Drucke das Dokument auf dem Tintendrucker.");
+      return "Drucke das Dokument auf dem Tintendrucker.";
+
+   }
+
 }
