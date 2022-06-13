04533570e79dab6edb7938226a9458a7a097e2f7
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-06-13 17:50:02 +0200

    inorder und postorder methode ergänzt

--- vorgaben/visitor_pattern/app/src/main/java/visitor_pattern/IVisitor.java
+++ vorgaben/visitor_pattern/app/src/main/java/visitor_pattern/IVisitor.java
@@ -3,5 +3,7 @@ package visitor_pattern;
 import visitor_pattern.tree.Node;
 
 public interface IVisitor {
+    public void visit(Inorder inorder);
+    public void visit(Postorder postorder);
     public void visit(Node node);
 }
