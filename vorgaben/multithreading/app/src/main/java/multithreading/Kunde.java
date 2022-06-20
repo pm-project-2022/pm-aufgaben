bec9c1a3dfcf76fe86469ee38ea3a3dbf39adf1e
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-06-20 02:58:20 +0200

    threading hinzugefügt

--- vorgaben/multithreading/app/src/main/java/multithreading/Kunde.java
+++ vorgaben/multithreading/app/src/main/java/multithreading/Kunde.java
@@ -1,10 +1,12 @@
 package multithreading;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 public class Kunde extends Thread{
     private Konto konto;
     private Queue<Rechnung> offeneRechnungen;
+    private final Logger logger = Logger.getLogger("Kunde-Logger");
 
     /** Erstelle einen neune Kunden, der sich ein Konto bei der Bank erstellt. */
     public Kunde() {
@@ -17,11 +19,31 @@ public class Kunde extends Thread{
      *
      * @param rechnung Die Rechnung.
      */
-    public void empfangeRechnung(Rechnung rechnung) {
+    public synchronized void empfangeRechnung(Rechnung rechnung) {
         offeneRechnungen.add(rechnung);
+        notifyAll();
     }
 
     public Konto getKonto() {
         return konto;
+    }
+
+    private synchronized void rechnungZahlen(){
+        while(offeneRechnungen.peek() == null){
+            try {
+                logger.warning("Es gibt keine zu bezahlende Rechnung in Thread " + Thread.currentThread().getName() + ". Es wird auf eine neue Rechnung gewartet.\n");
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        logger.info("Die oberste Rechnung von Thread " + Thread.currentThread().getName() + " wird bezahlt.\n");
+        Bank.ueberweisen(this.konto, this.offeneRechnungen.poll());
+        rechnungZahlen();
+    }
+
+    @Override
+    public void run() {
+        rechnungZahlen();
     }
 }
