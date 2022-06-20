38feca88665a6b07d0f5cf0135008eb395b0b6c4
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-06-20 09:02:30 +0200

    rechnungZahlen implementiert

--- vorgaben/multithreading/app/src/main/java/multithreading/Kunde.java
+++ vorgaben/multithreading/app/src/main/java/multithreading/Kunde.java
@@ -1,17 +1,31 @@
 package multithreading;
 
 import java.util.*;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
 import java.util.logging.Logger;
 
-public class Kunde extends Thread{
+public class Kunde implements Runnable{
     private Konto konto;
     private Queue<Rechnung> offeneRechnungen;
-    private final Logger logger = Logger.getLogger("Kunde-Logger");
+    private Logger kundenLogger = Logger.getLogger(Kunde.class.getSimpleName());
 
     /** Erstelle einen neune Kunden, der sich ein Konto bei der Bank erstellt. */
     public Kunde() {
-        konto = new Konto();
+        konto = new Konto(0);
         offeneRechnungen = new LinkedList<>();
+    }
+
+    public Kunde(int id) {
+        konto = new Konto(id);
+        offeneRechnungen = new LinkedList<>();
+        for (Handler handler : kundenLogger.getHandlers()) {
+            kundenLogger.removeHandler(handler);
+        }
+        kundenLogger.setUseParentHandlers(false);
+        ConsoleHandler ch = new ConsoleHandler();
+        ch.setFormatter(new LoggerFormatter());
+        kundenLogger.addHandler(ch);
     }
 
     /**
@@ -19,31 +33,47 @@ public class Kunde extends Thread{
      *
      * @param rechnung Die Rechnung.
      */
-    public synchronized void empfangeRechnung(Rechnung rechnung) {
+    public void empfangeRechnung(Rechnung rechnung) {
         offeneRechnungen.add(rechnung);
-        notifyAll();
+        kundenLogger.info("Konto-ID " + this.konto.getId() + ": Rechnung von Geldeintreiber " + rechnung.empfaenger().getId() + " bekommen.\nBetrag: " + rechnung.betrag() + " Euro");
+        invokeNotify();
     }
 
+    /**
+     * getter für das konto
+     * @return konto des kunden
+     */
     public Konto getKonto() {
         return konto;
     }
 
+    /**
+     * zahlt zahlt die oberste offene rechnung aus der queue offeneRechnung. ist die queue leer, wartet der thread
+     */
     private synchronized void rechnungZahlen(){
         while(offeneRechnungen.peek() == null){
             try {
-                logger.warning("Es gibt keine zu bezahlende Rechnung in Thread " + Thread.currentThread().getName() + ". Es wird auf eine neue Rechnung gewartet.\n");
+                kundenLogger.info("Alle Rechnung in Konto-ID " + this.konto.getId() + " bezahlt. Wait");
                 wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        logger.info("Die oberste Rechnung von Thread " + Thread.currentThread().getName() + " wird bezahlt.\n");
+        kundenLogger.info("Rechnung in Konto-ID " + this.konto.getId() + " wird versucht zu bezahlen");
         Bank.ueberweisen(this.konto, this.offeneRechnungen.poll());
-        rechnungZahlen();
+    }
+
+    /**
+     * entfernt alle threads aus der warteschlange
+     */
+    private synchronized void invokeNotify(){
+        notifyAll();
     }
 
     @Override
     public void run() {
-        rechnungZahlen();
+        while(true){
+            rechnungZahlen();
+        }
     }
 }
